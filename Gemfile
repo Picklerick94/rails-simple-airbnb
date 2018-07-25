d507bb889c47ad012cd61be5527735411f4f18e0
Jasmine Tang <jastangting@gmail.com>
2018-07-25 22:46:51 +0800

    simpale airbnb

--- Gemfile
+++ Gemfile
@@ -32,7 +32,12 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+  Flat.create(name: Faker::Address.city,
+              address: Faker::Address.street_address,
+              description: Faker::RickAndMorty.quote,
+              price_per_night: (1..5000).to_a.sample,
+              number_of_guests: (1..5).to_a.sample)
+end
+
+puts "Create #{Flat.count} flats"
